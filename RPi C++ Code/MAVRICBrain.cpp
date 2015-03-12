d2d252530e944e57c9ca986db2e5f4bc79df3da9
Drew May <drewm135@gmail.com>
2015-03-12 15:07:49 -0700

    Updated Brain code with presentation version

--- RPi C++ Code/MAVRICBrain.cpp
+++ RPi C++ Code/MAVRICBrain.cpp
@@ -4,8 +4,6 @@
 
 /*
  * Todo
- * Expand table to include all 14 data fields
- * Request proximity data
  * Request battery data
  * Request smell data
  * Request sound data
@@ -50,6 +48,8 @@
 
 #define LEFTSOUNDDATA               0xD
 #define RIGHTSOUNDDATA              0xE
+
+#define MOTORDATA                   0xF
 
 MYSQL *mysql1;
 
@@ -106,7 +106,7 @@ void mysql_create_table(void)
 	if(mysql1 != NULL)
 	{	
 		char final_query[1024];
-		sprintf(final_query, "CREATE TABLE %s (Time TIME, Temperature Double)", date);
+		sprintf(final_query, "CREATE TABLE %s (Time TIME, Temperature Double, Prox1 int, Prox2 int, Prox3 int, Prox4 int, Prox5 int, Prox6 int, Prox7 int, Prox8 int, BattLevel int, LeftMotor Int, RightMotor Int)", date);
 		if (mysql_query(mysql1, final_query)) {
 			fprintf(stderr, "%s\n", mysql_error(mysql1));
 			return;
@@ -116,26 +116,72 @@ void mysql_create_table(void)
 
 void mysql_write_temp(double temperature)
 {
-     if(mysql1 != NULL)
-     {
+    if(mysql1 != NULL)
+    {
 		char final_query[1024];
-		sprintf(final_query, "INSERT INTO %s VALUES(CURTIME(), %f)", date, temperature);
-		if (mysql_query(mysql1, final_query)) {
+		sprintf(final_query, "INSERT INTO %s (Time, Temperature) VALUES(CURTIME(), %f)", date, temperature);
+		if (mysql_query(mysql1, final_query)) 
+		{
 			fprintf(stderr, "%s\n", mysql_error(mysql1));
 			return;
 		}
-     }
+    }
 }
 
-int writeNumber(unsigned value) {
+void mysql_write_prox(int sensor, int proximity)
+{
+	if(mysql1 != NULL)
+	{
+		char final_query[1024];
+		sprintf(final_query, "INSERT INTO %s (Time, Prox%d) VALUES(CURTIME(), %d)", date, (sensor - 1), proximity);
+		if(mysql_query(mysql1, final_query))
+		{
+			fprintf(stderr, "%s\n", mysql_error(mysql1));
+			return;
+		}
+	}
+}
+
+void mysql_write_motor(char leftMotor, char rightMotor)
+{
+	if(mysql1 != NULL)
+	{
+		char final_query[1024];
+		sprintf(final_query, "INSERT INTO %s (Time, LeftMotor, RightMotor) VALUES(CURTIME(), %d, %d)", date, leftMotor, rightMotor);
+		if(mysql_query(mysql1, final_query))
+		{
+			fprintf(stderr, "%s\n", mysql_error(mysql1));
+			return;
+		}
+	}
+}
+
+void mysql_write_data(double temperature, int prox1, int prox2, int prox3, int prox4, int prox5, int prox6, int prox7, int prox8, int battery)
+{
+	if(mysql1 != NULL)
+	{
+		char final_query[1024];
+		sprintf(final_query, "INSERT INTO %s (Time, Temperature, Prox1, Prox2, Prox3, Prox4, Prox5, Prox6, Prox7, Prox8, BattLevel) VALUES(CURTIME(), %f, %d, %d, %d, %d, %d, %d, %d, %d, %d)", date, temperature, prox1, prox2, prox3, prox4, prox5, prox6, prox7, prox8, battery);
+		if(mysql_query(mysql1, final_query))
+		{
+			fprintf(stderr, "%s\n", mysql_error(mysql1));
+			return;
+		}
+	}
+}
+
+int writeNumber(unsigned value) 
+{
 	return wiringPiI2CWrite(fd, value);
 }
 
-int readNumber(void) {
+int readNumber(void) 
+{
 	return wiringPiI2CRead(fd);
 }
 
-void requestTemperature(void) {
+double requestTemperature(void) 
+{
 	int number;
 	double temp;
 	
@@ -145,19 +191,89 @@ void requestTemperature(void) {
 	number |= readNumber(); //Or number with new value
 	temp = 147.5 - ((225.0 * number) / 4095.0);
 	temp = ((temp * 9) + 160) / 5.0;
-	strftime(current_time, 50, "%r", localtime(&now));
-	mysql_write_temp(temp);
+	printf("\nTemp is %f", temp);
+	//mysql_write_temp(temp);
 }
 
-int main(int argc, const char * argv[]) {
+int requestBattData()
+{
+	int battery;
+	
+	writeNumber(BATTDATA); //Request battery data
+	battery = readNumber();
+	battery = battery << 8;
+	battery |= readNumber();
+	
+	return battery;	
+}
+
+int requestProx(int sensor) 
+{
+	int proximity;
+	
+	writeNumber(sensor); //Send the appropriate proximity constant
+	proximity = readNumber();
+	proximity = proximity << 8; //Bit shift number by one byte
+	proximity |= readNumber(); //Or proximity value with new value
+	printf("\nProx is %d", proximity);
+	//mysql_write_prox(sensor, proximity);	
+	return proximity;
+}
+
+void sendMotorCommand(char leftMotor, char rightMotor)
+{
+	//Motor value ranges from -100 to 100
+	writeNumber(MOTORDATA); //Inform Tiva that motor data is about to be sentry
+	writeNumber(leftMotor);
+	writeNumber(rightMotor);
+	mysql_write_motor(leftMotor, rightMotor);
+}
+
+int main(int argc, const char * argv[]) 
+{
 	now = time((time_t*)NULL);
 	mysql_connect();
 	mysql_create_table(); //Create the mysql table for this test
 	fd = wiringPiI2CSetup(address); // Automatically selects i2c on GPIO
+	int proximity[8] = {0}; //Make array of proximity values initialized to 0
+	
+	double temp;
+	int batterylevel;
 
 	while(1) {
-		requestTemperature(); //Request the current Tiva temperature
-		sleep(10); //Sleep for 10 seconds
+		temp = requestTemperature(); //Request the current Tiva temperature (2 bytes)
+		sleep(1);
+		proximity[0] = requestProx(PROX1DATA);       //Request data from Prox sensors (2 bytes)
+		sleep(1);
+		batterylevel = requestBattData();            //Request battery level (2 bytes)
+		/*//sleep(1);
+		proximity[1] = requestProx(PROX2DATA);
+		//sleep(1);
+		proximity[2] = requestProx(PROX3DATA);
+		//sleep(1);
+		proximity[3] = requestProx(PROX4DATA);
+		//sleep(1);
+		proximity[4] = requestProx(PROX5DATA);
+		//sleep(1);
+		proximity[5] = requestProx(PROX6DATA);
+		//sleep(1);
+		proximity[6] = requestProx(PROX7DATA);
+		//sleep(1);
+		proximity[7] = requestProx(PROX8DATA);
+		//sleep(1);*/
+		mysql_write_data(temp, proximity[0], proximity[1], proximity[2], proximity[3], proximity[4], proximity[5], proximity[6], proximity[7], batterylevel);
+		
+		//requestBattData();     //Request current battery level (2 bytes)
+		
+		//Stop motors if any proximity was below 25 centimetres
+		/*for (int i = 0; i < 8; i++) 
+		{
+			if (proximity[i] <= 25)
+			{
+				sendMotorCommand(0, 0); //Stop both motors (2 bytes)
+			}
+		}*/
+		sleep(1); //Sleep for 1 second
 	}
 	
 	mysql_disconnect(); //Will never reach actually
