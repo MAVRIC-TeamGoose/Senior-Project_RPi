e6f79170651e9ee2a5baab94cb592313299f2b44
Drew May <drewm135@gmail.com>
2015-06-04 22:45:56 -0700

    V1.0

--- RPi C++ Code/MAVRICBrain.cpp
+++ RPi C++ Code/MAVRICBrain.cpp
@@ -2,17 +2,11 @@
 // Author: Team Goose
 // Requires WiringPi http://wiringpi.com/
 
-/*
- * Todo
- * Request battery data
- * Request smell data
- * Request sound data
- */
-
 #include <iostream>
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 #include <time.h> //For sleeping
+#include <math.h>
 
 //MySQL Includes
 #include <stdio.h>
@@ -50,6 +44,18 @@
 #define RIGHTSOUNDDATA              0xE
 
 #define MOTORDATA                   0xF
+
+//****************************************************************************
+//
+// Math Constants for Thermistor
+//
+//****************************************************************************
+float DENOMINATOR = 0.000001790830963;
+
+int B = 3950;
+
+double VIN = 3.3;
+
 
 MYSQL *mysql1;
 
@@ -106,7 +112,7 @@ void mysql_create_table(void)
 	if(mysql1 != NULL)
 	{	
 		char final_query[1024];
-		sprintf(final_query, "CREATE TABLE %s (Time TIME, Temperature Double, Prox1 int, Prox2 int, Prox3 int, Prox4 int, Prox5 int, Prox6 int, Prox7 int, Prox8 int, BattLevel int, LeftMotor Int, RightMotor Int)", date);
+		sprintf(final_query, "CREATE TABLE %s (Time TIME, Temperature Double, Prox1 int, Prox2 int, Prox3 int, Prox4 int, Prox5 int, Prox6 int, Prox7 int, LSmellGood int, LSmellBad int, RSmellGood int, RSmellBad int, LSoundGood int, LSoundBad int, RSoundGood int, RSoundBad int, BattLevel int)", date);
 		if (mysql_query(mysql1, final_query)) {
 			fprintf(stderr, "%s\n", mysql_error(mysql1));
 			return;
@@ -114,54 +120,12 @@ void mysql_create_table(void)
 	}
 }
 
-void mysql_write_temp(double temperature)
-{
-    if(mysql1 != NULL)
-    {
-		char final_query[1024];
-		sprintf(final_query, "INSERT INTO %s (Time, Temperature) VALUES(CURTIME(), %f)", date, temperature);
-		if (mysql_query(mysql1, final_query)) 
-		{
-			fprintf(stderr, "%s\n", mysql_error(mysql1));
-			return;
-		}
-    }
-}
-
-void mysql_write_prox(int sensor, int proximity)
+void mysql_write_data(double temperature, int prox1, int prox2, int prox3, int prox4, int prox5, int prox6, int prox7, int LSmellGood, int LSmellBad, int RSmellGood, int RSmellBad, int LSoundGood, int LSoundBad, int RSoundGood, int RSoundBad, int battery)
 {
 	if(mysql1 != NULL)
 	{
 		char final_query[1024];
-		sprintf(final_query, "INSERT INTO %s (Time, Prox%d) VALUES(CURTIME(), %d)", date, (sensor - 1), proximity);
-		if(mysql_query(mysql1, final_query))
-		{
-			fprintf(stderr, "%s\n", mysql_error(mysql1));
-			return;
-		}
-	}
-}
-
-void mysql_write_motor(char leftMotor, char rightMotor)
-{
-	if(mysql1 != NULL)
-	{
-		char final_query[1024];
-		sprintf(final_query, "INSERT INTO %s (Time, LeftMotor, RightMotor) VALUES(CURTIME(), %d, %d)", date, leftMotor, rightMotor);
-		if(mysql_query(mysql1, final_query))
-		{
-			fprintf(stderr, "%s\n", mysql_error(mysql1));
-			return;
-		}
-	}
-}
-
-void mysql_write_data(double temperature, int prox1, int prox2, int prox3, int prox4, int prox5, int prox6, int prox7, int prox8, int battery)
-{
-	if(mysql1 != NULL)
-	{
-		char final_query[1024];
-		sprintf(final_query, "INSERT INTO %s (Time, Temperature, Prox1, Prox2, Prox3, Prox4, Prox5, Prox6, Prox7, Prox8, BattLevel) VALUES(CURTIME(), %f, %d, %d, %d, %d, %d, %d, %d, %d, %d)", date, temperature, prox1, prox2, prox3, prox4, prox5, prox6, prox7, prox8, battery);
+		sprintf(final_query, "INSERT INTO %s (Time, Temperature, Prox1, Prox2, Prox3, Prox4, Prox5, Prox6, Prox7, LSmellGood, LSmellBad, RSmellGood, RSmellBad, LSoundGood, LSoundBad, RSoundGood, RSoundBad, BattLevel) VALUES(CURTIME(), %f, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d)", date, temperature, prox1, prox2, prox3, prox4, prox5, prox6, prox7, LSmellGood, LSmellBad, RSmellGood, RSmellBad, LSoundGood, LSoundBad, RSoundGood, RSoundBad, battery);
 		if(mysql_query(mysql1, final_query))
 		{
 			fprintf(stderr, "%s\n", mysql_error(mysql1));
@@ -189,10 +153,9 @@ double requestTemperature(void)
 	number = readNumber();
 	number = number << 8; //Bit shift number by one byte
 	number |= readNumber(); //Or number with new value
-	temp = 147.5 - ((225.0 * number) / 4095.0);
-	temp = ((temp * 9) + 160) / 5.0;
+	temp = (double) B / log((((double) VIN / (number * 3.3 / 4096)) - 1) / DENOMINATOR); //Temp in Kelvin
+	temp = (temp - 273.15) * 1.8 + 32;
 	printf("\nTemp is %f", temp);
-	//mysql_write_temp(temp);
 }
 
 int requestBattData()
@@ -216,29 +179,53 @@ int requestProx(int sensor)
 	proximity = proximity << 8; //Bit shift number by one byte
 	proximity |= readNumber(); //Or proximity value with new value
 	printf("\nProx is %d", proximity);
-	//mysql_write_prox(sensor, proximity);	
 	return proximity;
 }
 
-void sendMotorCommand(char leftMotor, char rightMotor)
+int requestGoodAudio(int sensor)
 {
-	//Motor value ranges from -100 to 100
-	writeNumber(MOTORDATA); //Inform Tiva that motor data is about to be sentry
-	writeNumber(leftMotor);
-	writeNumber(rightMotor);
-	mysql_write_motor(leftMotor, rightMotor);
+	int goodAudio; //Bucket 1 or 3
+	writeNumber(sensor); //Send the appropriate audio constant
+	
+	goodAudio = (readNumber() << 24);
+	goodAudio |= (readNumber() << 16);
+	goodAudio |= (readNumber() << 8);
+	goodAudio |= readNumber();
+	
+	printf("\nGood Audio is %d", goodAudio);
+	
+	return goodAudio;
+}
+
+int requestBadAudio()
+{
+	int badAudio; //Bucket 2 or 4
+	
+	badAudio = (readNumber() << 24);
+	badAudio |= (readNumber() << 16);
+	badAudio |= (readNumber() << 8);
+	badAudio |= readNumber();
+	
+	printf("\nBad Audio is %d", badAudio);
+	
+	return badAudio;
+	
 }
 
 int main(int argc, const char * argv[]) 
 {
 	now = time((time_t*)NULL);
 	mysql_connect();
-	mysql_create_table(); //Create the mysql table for this test
+	mysql_create_table(); //Create the mysql table for this test	
+	
 	fd = wiringPiI2CSetup(address); // Automatically selects i2c on GPIO
 	int proximity[8] = {0}; //Make array of proximity values initialized to 0
+	int audioData[8] = {0}; //Make array of audio values initialized to 0
 	
 	double temp;
 	int batterylevel;
+	
+	int sendData = 0;
 
 	while(1) {
 		temp = requestTemperature(); //Request the current Tiva temperature (2 bytes)
@@ -249,12 +236,29 @@ int main(int argc, const char * argv[])
 		proximity[4] = requestProx(PROX5DATA);
 		proximity[5] = requestProx(PROX6DATA);
 		proximity[6] = requestProx(PROX7DATA);
-		proximity[7] = requestProx(PROX8DATA);
-		requestBattData();
-		mysql_write_data(temp, proximity[0], proximity[1], proximity[2], proximity[3], proximity[4], proximity[5], proximity[6], proximity[7], batterylevel);
-		sleep(10);
+		
+		audioData[0] = requestGoodAudio(LEFTSMELLDATA);
+		audioData[1] = requestBadAudio();
+		
+		audioData[2] = requestGoodAudio(RIGHTSMELLDATA);
+		audioData[3] = requestBadAudio();
+		
+		audioData[4] = requestGoodAudio(LEFTSOUNDDATA);
+		audioData[5] = requestBadAudio();
+		
+		audioData[6] = requestGoodAudio(RIGHTSOUNDDATA);
+		audioData[7] = requestBadAudio();
+		
+		batterylevel = requestBattData();
+		sendData++;
+		
+		if (sendData >= 50){
+			sendData = 0;
+			//mysql_write_data(temp, proximity[0], proximity[1], proximity[2], proximity[3], proximity[4], proximity[5], proximity[6], batterylevel);
+			mysql_write_data(temp, proximity[0], proximity[1], proximity[2], proximity[3], proximity[4], proximity[5], proximity[6], audioData[0], audioData[1], audioData[2], audioData[3], audioData[4], audioData[5], audioData[6], audioData[7], batterylevel);
+		}
+		usleep(100000); //Sleep for 0.1 seconds
 	}
-	
 	mysql_disconnect(); //Will never reach actually
 	
 	return 0;
